c14738b20e3dd7f92c05384311fcd77fd7184c44
Brian Serumaga <robinahkasoma@Robinahs-iMac.fios-router.home>
2018-07-16 18:22:07 -0400

    Feature(Components): Added the basic required components.

---
+++ src/components/common/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Header = () => (
+    <div>
+        <div className="info">
+        heaf
+            <Link to=""></Link>
+        </div>
+    </div> 
+)
---
+++ src/components/textbox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Textbox extends Component {
+    render(){
+        return (
+            <div>
+                Hello Textbox
+            </div>
+        )
+    }
+}
