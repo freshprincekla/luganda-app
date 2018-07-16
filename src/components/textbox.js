86fbe5669bcd748ec484507fcffb31a0baeb543b
Brian Serumaga <robinahkasoma@Robinahs-iMac.fios-router.home>
2018-07-16 19:41:43 -0400

    ft(textbox): Added textbox  - Added textbox element.  -  Added onchange method to update local state on user input.

--- src/components/textbox.js
+++ src/components/textbox.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 export class Textbox extends Component {
+    constructor(props){
+        super(props);
+        this.state = {term: ""};
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+    onInputChange(term){
+        this.setState({term})
+    }
     render(){
         return (
             <div>
-                Hello Textbox
+                <input 
+                    type="text"
+                    id="term"
+                    value={this.state.term}
+                    placeholder = "Search bucket list items."
+                    onChange={event => this.onInputChange(event.target.value)}
+                    required 
+                />
             </div>
         )
     }
