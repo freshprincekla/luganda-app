9469a8268fe61ab2471f69cff1c457b74f5528cf
Brian Serumaga <robinahkasoma@Robinahs-iMac.fios-router.home>
2018-07-17 12:54:13 -0400

    Feature(Select): Added select button  - Added select input  - Added onchange handler on parent component to change state  - Added a onchange handler on child with callback

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import { BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store } from './store';
-import { Header } from './components/common/header';
+import { Header } from './components/header/header';
 import { Logo } from './components/logo';
 import { Textbox } from './components/textbox';
+import { ButtonGrid } from './components/buttongrid';
 
 class App extends Component {
   //Main application component
+  constructor(){
+    super();
+    this.state = { english: false};
+    this.handleLanguageChange = this.handleLanguageChange.bind(this);
+  }
+  handleLanguageChange(english){
+    this.setState({ english })
+  }
   render() {
     return (
       <Provider store={store}>
       <BrowserRouter>
         <div className="App">
-          <Header />
+          <Header handleLanguageChange={this.handleLanguageChange}/>
           <Logo />
           <Textbox />
+          <ButtonGrid english={this.state.english} />
         </div>
       </BrowserRouter>
       </Provider>
